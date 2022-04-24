2ce4ad14b4f9a3cd9a5f73a351b5541ce12c7f05
shine-jung <79754613+shine-jung@users.noreply.github.com>
2022-04-24 23:56:08 +0900

    lecture 7.1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ function App() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") return;
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
-  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -24,6 +22,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
